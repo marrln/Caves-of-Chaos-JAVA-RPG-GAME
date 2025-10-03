2c2354bd38c4b3ee5711c709107263b969f2bb8b
Maria Eleni Patsi <mrlnpts@outlook.com.gr>
2025-10-03 11:11:41 +0300

    Refactor EnemyStats class to streamline attributes and enhance enemy configuration

--- src/config/EnemyConfig.java
+++ src/config/EnemyConfig.java
@@ -5,88 +5,123 @@ import enemies.EnemyType;
 public class EnemyConfig {
 
     public static class EnemyStats {
-        public final int baseHp, baseDamage, expReward, movementSpeed, 
-                        attackCooldown, noticeRadius, diceSides, 
-                        variationPercent;
-        public final int[] attackChances, attackDamageMultipliers;
+        public final int baseHp, expReward, movementSpeed, attackCooldown, noticeRadius;
+        public final int[] attackChances;
+        public final int[] attackDice;      // number of dice per attack
+        public final int[] attackDiceSides; // sides per die
+        public final int[] attackModifiers; // flat bonus per attack
 
-        public EnemyStats(int baseHp, int baseDamage, int expReward, int movementSpeed,
-                        int attackCooldown, int noticeRadius, int diceSides,
-                        int variationPercent, 
-                        int[] attackChances, int[] attackDamageMultipliers) {
+        public EnemyStats(int baseHp, int expReward, int movementSpeed, int attackCooldown, 
+                        int noticeRadius,
+                        int[] attackChances, 
+                        int[] attackDice, 
+                        int[] attackDiceSides, 
+                        int[] attackModifiers) {
             this.baseHp = baseHp;
-            this.baseDamage = baseDamage;
             this.expReward = expReward;
             this.movementSpeed = movementSpeed;
             this.attackCooldown = attackCooldown;
             this.noticeRadius = noticeRadius;
-            this.diceSides = diceSides;
-            this.variationPercent = variationPercent;
             this.attackChances = attackChances;
-            this.attackDamageMultipliers = attackDamageMultipliers;
+            this.attackDice = attackDice;
+            this.attackDiceSides = attackDiceSides;
+            this.attackModifiers = attackModifiers;
         }
     }
 
     public static EnemyStats getStats(EnemyType type) {
         return switch (type) {
-            case SLIME -> new EnemyStats(15, 3, 5, 1,
-                                        2200, 4, 6, 
-                                        30,
-                                        new int[]{70, 30}, new int[]{100, 150});
-
-            case ORC -> new EnemyStats(25, 5, 10, 1,
-                                    2000, 5, 6, 
-                                    25,
-                                    new int[]{60, 40}, new int[]{100, 140});
-
-            case SKELETON -> new EnemyStats(20, 4, 8, 1,
-                                            2000, 6, 6, 
-                                            20,
-                                            new int[]{75, 25}, new int[]{100, 120});
-
-            case WEREWOLF -> new EnemyStats(30, 6, 15, 2,
-                                            2000, 7, 8, 
-                                            35,
-                                            new int[]{50, 50}, new int[]{100, 130});
-
-            case ARMORED_ORC -> new EnemyStats(40, 7, 20, 1,
-                                            2000, 5, 8, 
-                                            30,
-                                            new int[]{50, 30, 20}, new int[]{100, 140, 110});
-
-            case ARMORED_SKELETON -> new EnemyStats(35, 6, 18, 1,
-                                                    1100, 6, 8, 
-                                                    25,
-                                                    new int[]{60, 25, 15}, new int[]{100, 120, 160});
-
-            case ELITE_ORC -> new EnemyStats(45, 8, 25, 1,
-                                            2000, 6, 10, 
-                                            40,
-                                            new int[]{40, 35, 25}, new int[]{100, 130, 170});
-
-            case GREATSWORD_SKELETON -> new EnemyStats(50, 9, 30, 1,
-                                                    2000, 5, 12, 
-                                                    45,
-                                                    new int[]{30, 40, 30}, new int[]{100, 150, 200});
-
-            case ORC_RIDER -> new EnemyStats(38, 7, 22, 2,
-                                            2000, 8, 8, 
-                                            35,
-                                            new int[]{45, 35, 20}, new int[]{100, 120, 160});
-
-            case WEREBEAR -> new EnemyStats(60, 10, 35, 1,
-                                        2000, 6, 12, 
-                                        50,
-                                        new int[]{40, 35, 25}, new int[]{100, 140, 180});
-
-            case MEDUSA_OF_CHAOS -> new EnemyStats(200, 15, 100, 1,
-                                                2000, 10, 20, 
-                                                60,
-                                                new int[]{30, 35, 35}, new int[]{100, 160, 220});
+            case SLIME -> new EnemyStats(
+                8, 100, 1, 
+                2200, 3,
+                new int[]{60, 40, 0}, // attackChances
+                new int[]{1, 1, 0},   // attackDice
+                new int[]{6, 8, 0},   // attackDiceSides
+                new int[]{0, 0, 0}    // attackModifiers
+            );
+            case ORC -> new EnemyStats(
+                18, 100, 1, 
+                2000, 6,
+                new int[]{50, 50, 0}, // attackChances
+                new int[]{2, 2, 0},   // attackDice
+                new int[]{6, 6, 0},   // attackDiceSides
+                new int[]{0, 5, 0}    // attackModifiers
+            );
+            case SKELETON -> new EnemyStats(
+                30, 200, 1, 
+                2000, 5,
+                new int[]{50, 50, 0}, // attackChances
+                new int[]{1, 1, 0},   // attackDice
+                new int[]{8, 8, 0},   // attackDiceSides
+                new int[]{0, 5, 0}    // attackModifiers
+            );
+            case ARMORED_SKELETON -> new EnemyStats(
+                55, 200, 1, 
+                1100, 6,
+                new int[]{60, 40, 0}, // attackChances
+                new int[]{2, 2, 0},   // attackDice
+                new int[]{6, 6, 0},   // attackDiceSides
+                new int[]{0, 10, 0}   // attackModifiers
+            );
+            case ARMORED_ORC -> new EnemyStats(
+                45, 500, 1, 
+                2000, 6,
+                new int[]{40, 40, 20}, // attackChances
+                new int[]{2, 2, 3},    // attackDice
+                new int[]{6, 6, 6},    // attackDiceSides
+                new int[]{2, 2, 2}     // attackModifiers
+            );
+            case GREATSWORD_SKELETON -> new EnemyStats(
+                65, 500, 1, 
+                2000, 6,
+                new int[]{40, 40, 20}, // attackChances
+                new int[]{3, 3, 4},    // attackDice
+                new int[]{6, 6, 6},    // attackDiceSides
+                new int[]{2, 0, 5}     // attackModifiers
+            );
+            case WEREWOLF -> new EnemyStats(
+                70, 800, 2, 
+                2000, 5,
+                new int[]{50, 50, 0}, // attackChances
+                new int[]{4, 4, 0},   // attackDice
+                new int[]{6, 6, 0},   // attackDiceSides
+                new int[]{5, 10, 0}   // attackModifiers
+            );
+            case WEREBEAR -> new EnemyStats(
+                100, 1000, 1, 
+                2000, 8,
+                new int[]{40, 40, 20}, // attackChances
+                new int[]{2, 3, 4},    // attackDice
+                new int[]{6, 6, 6},    // attackDiceSides
+                new int[]{10, 0, 15}   // attackModifiers
+            );
+            case ELITE_ORC -> new EnemyStats(
+                200, 1500, 1, 
+                2000, 8,
+                new int[]{40, 40, 20}, // attackChances
+                new int[]{5, 4, 4},    // attackDice
+                new int[]{6, 6, 6},    // attackDiceSides
+                new int[]{10, 0, 5}    // attackModifiers
+            );
+            case ORC_RIDER -> new EnemyStats(
+                280, 1500, 2, 
+                2000, 8,
+                new int[]{40, 40, 20}, // attackChances
+                new int[]{5, 4, 4},    // attackDice
+                new int[]{7, 7, 7},    // attackDiceSides
+                new int[]{10, 2, 5}    // attackModifiers
+            );
+            case MEDUSA_OF_CHAOS -> new EnemyStats(
+                1500, 10000, 1, 
+                2000, 10,
+                new int[]{100, 0, 0}, // attackChances
+                new int[]{10, 0, 0},  // attackDice
+                new int[]{6, 0, 0},   // attackDiceSides
+                new int[]{10, 0, 0}   // attackModifiers
+            );
         };
     }
 
-    /** Base movement cooldown in milliseconds (can be modified by movement speed). */
     public static int getBaseMovementCooldown() {
         return 800;
     }
