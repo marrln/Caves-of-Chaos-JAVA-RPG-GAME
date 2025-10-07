5efe9c826ee866f0dd8bd97f77ae9da328d4cc32
Maria Eleni Patsi <mrlnpts@outlook.com.gr>
2025-10-07 14:58:06 +0300

    condense code

--- src/config/EnemySpawnConfig.java
+++ src/config/EnemySpawnConfig.java
@@ -24,44 +24,20 @@ public class EnemySpawnConfig {
     static { initializeLevelConfigs(); }
 
     private static void initializeLevelConfigs() {
-        LEVEL_CONFIGS.put(1, new LevelSpawnConfig(Map.of(
-                EnemyType.SLIME, 3, EnemyType.SKELETON, 2)));
-
-        LEVEL_CONFIGS.put(2, new LevelSpawnConfig(Map.of(
-                EnemyType.SLIME, 2, EnemyType.ORC, 3, EnemyType.SKELETON, 2)));
-
-        LEVEL_CONFIGS.put(3, new LevelSpawnConfig(Map.of(
-                EnemyType.SLIME, 2, EnemyType.ORC, 2, EnemyType.SKELETON, 2, EnemyType.WEREWOLF, 1)));
-
-        LEVEL_CONFIGS.put(4, new LevelSpawnConfig(Map.of(
-                EnemyType.ARMORED_ORC, 2, EnemyType.ARMORED_SKELETON, 2,
-                EnemyType.WEREWOLF, 1, EnemyType.ORC, 1)));
-
-        LEVEL_CONFIGS.put(5, new LevelSpawnConfig(Map.of(
-                EnemyType.ARMORED_ORC, 2, EnemyType.GREATSWORD_SKELETON, 2, EnemyType.WEREWOLF, 1)));
-
-        LEVEL_CONFIGS.put(6, new LevelSpawnConfig(Map.of(
-                EnemyType.ELITE_ORC, 1, EnemyType.ARMORED_ORC, 2, EnemyType.WEREBEAR, 1)));
-
-        LEVEL_CONFIGS.put(7, new LevelSpawnConfig(Map.of(
-                EnemyType.ORC_RIDER, 1, EnemyType.ELITE_ORC, 1,
-                EnemyType.ARMORED_SKELETON, 2, EnemyType.WEREBEAR, 1)));
-
-        LEVEL_CONFIGS.put(8, new LevelSpawnConfig(Map.of(
-                EnemyType.ARMORED_ORC, 2, EnemyType.GREATSWORD_SKELETON, 2, EnemyType.WEREBEAR, 1)));
-
-        LEVEL_CONFIGS.put(9, new LevelSpawnConfig(Map.of(
-                EnemyType.ELITE_ORC, 2, EnemyType.ORC_RIDER, 1, EnemyType.WEREBEAR, 1)));
-
-        LEVEL_CONFIGS.put(10, new LevelSpawnConfig(Map.of(
-                EnemyType.MEDUSA_OF_CHAOS, 1), true));
+        LEVEL_CONFIGS.put(1, new LevelSpawnConfig(Map.of(EnemyType.SLIME, 3, EnemyType.SKELETON, 2)));
+        LEVEL_CONFIGS.put(2, new LevelSpawnConfig(Map.of(EnemyType.SLIME, 2, EnemyType.ORC, 3, EnemyType.SKELETON, 2)));
+        LEVEL_CONFIGS.put(3, new LevelSpawnConfig(Map.of(EnemyType.SLIME, 2, EnemyType.ORC, 2, EnemyType.SKELETON, 2, EnemyType.WEREWOLF, 1)));
+        LEVEL_CONFIGS.put(4, new LevelSpawnConfig(Map.of(EnemyType.ARMORED_ORC, 2, EnemyType.ARMORED_SKELETON, 2, EnemyType.WEREWOLF, 1, EnemyType.ORC, 1)));
+        LEVEL_CONFIGS.put(5, new LevelSpawnConfig(Map.of(EnemyType.ARMORED_ORC, 2, EnemyType.GREATSWORD_SKELETON, 2, EnemyType.WEREWOLF, 1)));
+        LEVEL_CONFIGS.put(6, new LevelSpawnConfig(Map.of(EnemyType.ELITE_ORC, 1, EnemyType.ARMORED_ORC, 2, EnemyType.WEREBEAR, 1)));
+        LEVEL_CONFIGS.put(7, new LevelSpawnConfig(Map.of(EnemyType.ORC_RIDER, 1, EnemyType.ELITE_ORC, 1, EnemyType.ARMORED_SKELETON, 2, EnemyType.WEREBEAR, 1)));
+        LEVEL_CONFIGS.put(8, new LevelSpawnConfig(Map.of(EnemyType.ARMORED_ORC, 2, EnemyType.GREATSWORD_SKELETON, 2, EnemyType.WEREBEAR, 1)));
+        LEVEL_CONFIGS.put(9, new LevelSpawnConfig(Map.of(EnemyType.ELITE_ORC, 2, EnemyType.ORC_RIDER, 1, EnemyType.WEREBEAR, 1)));
+        LEVEL_CONFIGS.put(10, new LevelSpawnConfig(Map.of(EnemyType.MEDUSA_OF_CHAOS, 1), true));
     }
 
     public static LevelSpawnConfig getSpawnConfig(int level) { return LEVEL_CONFIGS.get(level); }
-
-    public static int getMaxLevel() {
-        return LEVEL_CONFIGS.keySet().stream().mapToInt(Integer::intValue).max().orElse(1);
-    }
+    public static int getMaxLevel() { return LEVEL_CONFIGS.keySet().stream().mapToInt(Integer::intValue).max().orElse(1); }
 
     public static boolean isBossLevel(int level) {
         LevelSpawnConfig cfg = getSpawnConfig(level);
