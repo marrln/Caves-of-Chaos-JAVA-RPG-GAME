4e3f740c44d77930c9ef67e0e728e9b578e00247
Maria Eleni Patsi <mrlnpts@outlook.com.gr>
2025-10-09 16:47:12 +0300

    Update trap damage values

--- src/items/PoisonDartTrap.java
+++ src/items/PoisonDartTrap.java
@@ -10,7 +10,7 @@ public class PoisonDartTrap extends AbstractTrap {
     private static final String TRAP_NAME = "Poison Dart Trap";
     private static final String DESCRIPTION = "Ancient mechanisms hidden in the walls that shoot poisoned darts when triggered.";
     private static final String TRIGGER_MSG = "A poisoned dart shoots out from the wall and strikes you!";
-    private static final String DAMAGE_MSG = "The poison courses through your veins, sapping your strength.";
+    private static final String DAMAGE_MSG = "The poison courses through your veins making you feel weak.";
     
     public PoisonDartTrap() {
         super(TRAP_NAME, DESCRIPTION, MODERATE_DAMAGE, TRIGGER_MSG, DAMAGE_MSG);
