beca5f678cccd0089e4c11e2bfc30b4fc3f36c05
Maria Eleni Patsi <mrlnpts@outlook.com.gr>
2025-10-10 10:17:57 +0300

    Remove redundant comments

--- src/enemies/Enemy.java
+++ src/enemies/Enemy.java
@@ -4,69 +4,15 @@ package enemies;
  * Base interface for all enemies in the game.
  */
 public interface Enemy {
-    
-    /**
-     * Gets the enemy's current x position.
-     * 
-     * @return The x position
-     */
+
     int getX();
-    
-    /**
-     * Gets the enemy's current y position.
-     * 
-     * @return The y position
-     */
     int getY();
-    
-    /**
-     * Sets the enemy's position.
-     * 
-     * @param x The new x position
-     * @param y The new y position
-     */
     void setPosition(int x, int y);
-    
-    /**
-     * Gets the enemy's current health.
-     * 
-     * @return The current HP
-     */
     int getHp();
-    
-    /**
-     * Gets the enemy's maximum health.
-     * 
-     * @return The max HP
-     */
     int getMaxHp();
-    
-    /**
-     * Sets the enemy's health to a new value.
-     * 
-     * @param hp The new health value
-     */
     void setHp(int hp);
-    
-    /**
-     * Gets the name of the enemy.
-     * 
-     * @return The enemy's name
-     */
     String getName();
-    
-    /**
-     * Gets the amount of experience rewarded when this enemy is defeated.
-     * 
-     * @return The experience reward
-     */
     int getExpReward();
-    
-    /**
-     * Checks if the enemy is dead.
-     * 
-     * @return true if the enemy is dead, false otherwise
-     */
     boolean isDead();
     
     /**
